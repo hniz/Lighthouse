edf77d22d09725a1ca5b3072e28a4d01df6ecc63
smax253 <smax253@gmail.com>
2020-11-17 12:57:16 -0500

    Adds Home Page Route and base Handlebars Template [Resolves #1]

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,7 @@
 const routeConstructor = (app) => {
+    app.get('/', (req, res) => {
+        res.render('home_page', { title: 'Lighthouse' });
+    });
     app.use('*', (req, res) => {
         res.status(404).render('not_found', { title: 'Page not found!' });
     });
