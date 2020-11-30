c512c94cb77d0c35d9da3e0aca06cf7501d06266
smax253 <smax253@gmail.com>
2020-11-29 21:44:57 -0500

    Added MODIFY user database function [Resolves #32]

--- src/data/users.js
+++ src/data/users.js
@@ -1,13 +1,16 @@
 const collections = require('../config/mongoCollections');
 const checkUserInfo = require('../helpers/check_user_info');
-const posts = require('./posts')
-const comments = require('./comments')
-const classes = require('./classes')
+const posts = require('./posts');
+const comments = require('./comments');
+const classes = require('./classes');
 const bcrypt = require('bcrypt');
+const { checkUpdatedUserInfo } = require('../helpers/check_user_info');
 
 const create = async ({ email, firstName, lastName, password, type }) => {
     const users = await collections.users();
-    if (checkUserInfo.checkUserInfo(email, password, firstName, lastName, type)) {
+    if (
+        checkUserInfo.checkUserInfo(email, password, firstName, lastName, type)
+    ) {
         const userLookup = await users.findOne({ email });
         if (!userLookup) {
             users.insertOne({
@@ -39,49 +42,119 @@ const create = async ({ email, firstName, lastName, password, type }) => {
     }
 };
 
-const deleteUser = async({ email }) => {  //Notes: Should we get password confirmation to delete a user? 
+const getUserByEmail = async ({ email }) => {
     const users = await collections.users();
-    const userLookup = await users.findOne({ email })
-
-    if(!userLookup){ 
-        return { 
-            error: "Delete user failed: user does not exist.",
-            statusCode: 400
+    const userLookup = await users.findOne({ email });
+    if (!userLookup) {
+        return { error: 'No user with given email', statusCode: 404 };
+    } else {
+        return {
+            user: userLookup,
+            statusCode: 200,
         };
-    } else { 
-        if(!checkUserInfo.validateEmail(email)){ 
-            return { 
-                error: "Invalid email provided for deletion.",
-                statusCode: 400
-            }
+    }
+};
+
+const getUserByToken = async ({ token }) => {
+    const users = await collections.users();
+    const userLookup = await users.findOne({ token });
+    if (!userLookup) {
+        return { error: 'No user with given email', statusCode: 404 };
+    } else {
+        return {
+            user: userLookup,
+            statusCode: 200,
+        };
+    }
+};
+
+const modifyUser = async ({
+    email,
+    firstName,
+    lastName,
+    password,
+    type,
+    token,
+}) => {
+    const users = await collections.users();
+    const changedFields = checkUpdatedUserInfo({
+        firstName,
+        lastName,
+        password,
+        type,
+        token,
+    });
+    if (changedFields.error) {
+        return {
+            error: changedFields.error,
+            statusCode: 400,
+        };
+    }
+    const result = await users.findOneAndUpdate(
+        {
+            email,
+        },
+        { $set: changedFields },
+    );
+    if (result.ok !== 1) {
+        return {
+            error: 'Error updating fields.',
+            statusCode: 500,
+        };
+    } else {
+        return {
+            statusCode: 200,
+        };
+    }
+};
+
+const deleteUser = async ({ email }) => {
+    //Notes: Should we get password confirmation to delete a user?
+    const users = await collections.users();
+    const userLookup = await users.findOne({ email });
+
+    if (!userLookup) {
+        return {
+            error: 'Delete user failed: user does not exist.',
+            statusCode: 400,
+        };
+    } else {
+        if (!checkUserInfo.validateEmail(email)) {
+            return {
+                error: 'Invalid email provided for deletion.',
+                statusCode: 400,
+            };
         }
         //Need to delete the posts, comments and users collection as well.
         const postsArray = userLookup.posts;
-        postsArray.forEach(member => { 
-            posts.deletePost(member, userLookup._id) 
-        })
+        postsArray.forEach((member) => {
+            posts.deletePost(member, userLookup._id);
+        });
         const commentsArray = userLookup.comments;
-        commentsArray.forEach(member => { 
-            comments.deleteUserComments(member, userLookup._id)
-        })
+        commentsArray.forEach((member) => {
+            comments.deleteUserComments(member, userLookup._id);
+        });
         const classesArray = userLookup.classes;
-        classesArray.forEach(member => { 
-            classes.deleteClassesFromUser(member, userLookup._id)
-        })
-        const deleteInfo = await users.deleteOne({email: email})
-        if(deleteInfo.deletedCount === 0){ 
-            return { 
-                error: "Failed to delete user.",
-                statusCode: 404
+        classesArray.forEach((member) => {
+            classes.deleteClassesFromUser(member, userLookup._id);
+        });
+        const deleteInfo = await users.deleteOne({ email: email });
+        if (deleteInfo.deletedCount === 0) {
+            return {
+                error: 'Failed to delete user.',
+                statusCode: 404,
             };
         }
-        return { 
-            statusCode: 204
-        }
+        return {
+            statusCode: 204,
+        };
     }
-}
+};
 
 module.exports = {
     create,
-    deleteUser
+    deleteUser,
+    getUserByEmail,
+    getUserByToken,
+    modifyUser,
 };
--- src/helpers/check_user_info.js
+++ src/helpers/check_user_info.js
@@ -1,19 +1,22 @@
 const emailValidator = require('email-validator');
 
 const checkUserInfo = (email, password, firstName, lastName, type) => {
-    if (
-        !email ||
-        typeof email !== 'string' ||
-        !email.trim() ||
-        !emailValidator.validate(email)
-    )
+    if (!validateEmail(email)) return false;
+    if (!validateString(password)) return false;
+    if (!validateString(firstName)) return false;
+    if (!validateString(lastName)) return false;
+    if (!validateType(type)) return false;
+    return true;
+};
+
+const validateString = (string) => {
+    if (!string || typeof string !== 'string' || !string.trim()) {
         return false;
-    if (!password || typeof password !== 'string' || !password.trim())
-        return false;
-    if (!firstName || typeof firstName !== 'string' || !firstName.trim())
-        return false;
-    if (!lastName || typeof lastName !== 'string' || !lastName.trim())
-        return false;
+    }
+    return true;
+};
+
+const validateType = (type) => {
     if (
         !type ||
         typeof type !== 'string' ||
@@ -23,7 +26,7 @@ const checkUserInfo = (email, password, firstName, lastName, type) => {
     return true;
 };
 
-const validateEmail = (email) => { 
+const validateEmail = (email) => {
     if (
         !email ||
         typeof email !== 'string' ||
@@ -32,9 +35,51 @@ const validateEmail = (email) => {
     )
         return false;
     return true;
-}
+};
+const checkUpdatedUserInfo = ({
+    firstName,
+    lastName,
+    password,
+    type,
+    token,
+}) => {
+    const changedFields = {};
+    let errors = [];
+    if (firstName) {
+        if (validateString(firstName)) changedFields.firstName = firstName;
+        else {
+            errors.push('first name');
+        }
+    }
+    if (lastName) {
+        if (validateString(lastName)) changedFields.lastName = lastName;
+        else {
+            errors.push('last name');
+        }
+    }
+    if (password) {
+        if (validateString(password)) changedFields.password = password;
+        else {
+            errors.push('password');
+        }
+    }
+    if (type) {
+        if (validateType(type)) changedFields.type = type;
+        else {
+            errors.push('type');
+        }
+    }
+    if (token) changedFields.token = token;
+    if (errors.length > 0) {
+        const errorString = `Invalid ${errors.join(', ')}.`;
+        return { error: errorString };
+    } else return changedFields;
+};
 
-module.exports = { 
+module.exports = {
     checkUserInfo,
-    validateEmail
+    checkUpdatedUserInfo,
+    validateEmail,
+    validateString,
+    validateType,
 };
