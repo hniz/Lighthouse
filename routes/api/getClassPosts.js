212859c3405a75ba7ccae7b5fc5c6378774b4150
smax253 <smax253@gmail.com>
2020-12-06 22:09:44 -0500

    hamzah slow vscode pains

--- routes/api/getClassPosts.js
+++ routes/api/getClassPosts.js
@@ -2,36 +2,42 @@ const e = require('express');
 const classes = require('../../data/classes');
 const Router = e.Router();
 
-Router.get('/', async(req, res) => {
+Router.get('/', async (req, res) => {
     const classId = req.query.id;
-    try{
+    try {
         const result = await classes.getClassById(classId);
-        if(result.error){
-            res.status(result.statusCode).render('partials/class_not_found', { layout: null });
+        if (result.error) {
+            res.status(result.statusCode).render('partials/class_not_found', {
+                layout: null,
+            });
         }
         const fetchedClass = result.class;
-        if(fetchedClass.posts.length === 0){
-            res.send('<p> No posts for this class found.</p>'); //maybe an error message here of no posts available 
+        if (fetchedClass.posts.length === 0) {
+            res.send('<p> No posts for this class found.</p>'); //maybe an error message here of no posts available
         }
-        const { classPosts } = await classes.getClassPosts(req.body.id);
-        let postData = { postInfo: [ ] };
-        console.log(classPosts);
-        classPosts.forEach(post =>{
-            postData.postInfo.push({
+        const getClassPostsResult = await classes.getClassPosts(req.query.id);
+        if (getClassPostsResult.error) {
+            res.status(500).send(
+                '<p> Sorry, there was an error fetching the class posts.</p>'
+            );
+            return;
+        }
+        let postData = [];
+        getClassPostsResult.classPosts.forEach(({ post }) => {
+            postData.push({
                 title: post.title,
-                ids: post._id.str,
+                ids: post._id.toString(),
             });
         });
-        res.render('display_class_posts', {
+        console.log(postData);
+        res.render('partials/display_class_posts', {
             data: postData,
         });
-
-    } catch(e){
-        console.log(e);
-        res.status(500).send('<p> Sorry, there was an error fetching the class posts.</p>');
+    } catch (e) {
+        res.status(500).send(
+            '<p> Sorry, there was an error fetching the class posts.</p>'
+        );
     }
-    
-
 });
 
 module.exports = Router;
