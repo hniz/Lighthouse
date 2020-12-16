0e0d88702e04a3316028350d0d933cab607d3f12
Hamzah Nizami <hnizami1@stevens.edu>
2020-12-16 00:46:04 -0500

    Adds class enrollment information to user profile. [Resolves #89]

--- routes/user/view_profile.js
+++ routes/user/view_profile.js
@@ -1,10 +1,12 @@
 const e = require('express');
 const { getUserById, getUserByToken } = require('../../data/users');
+const { getClassById } = require('../../data/classes');
 const Router = e.Router();
 
 const displayProfile = async (req, res) => {
     const userId = req.params.id;
     const userLookup = await getUserById(userId);
+    
     if (userLookup.error) {
         res.status(userLookup.statusCode).render('profile', {
             title: 'Error',
@@ -12,11 +14,18 @@ const displayProfile = async (req, res) => {
             loggedIn: req.session.token ? true : false,
         });
     } else {
+        const usersClasses = userLookup.user.classes;
+        let classes = [];
+        for(let i = 0; i < usersClasses.length; i++) {
+            const getClass = await getClassById(usersClasses[i]);
+            classes.push(getClass.class.name);
+        }
         const user = userLookup.user;
         user.fullName = user.fullName.firstName + ' ' + user.fullName.lastName;
         res.status(userLookup.statusCode).render('profile', {
             title: `${user.fullName}'s Profile`,
             user,
+            userClasses: classes,
             loggedIn: req.session.token ? true : false,
         });
     }
