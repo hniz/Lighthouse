1761c9580e69d515b98103b4d13dd54c1fa25b4d
Eric Altenburg <ealtenbu@stevens.edu>
2020-12-15 20:10:24 -0500

    WIP, skeleton of validating the new class form mainly done.

---
+++ public/js/scripts/new_class.js
@@ -0,0 +1,7 @@
+jQuery(function ($) {
+    let myForm = document.getElementById('new-class-form');
+    let inputClassName = document.getElementById('class-name');
+    let inputClassDescription = document.getElementById('class-description');
+    let errorDiv = document.getElementById('new-class-error');
+    let errorUL = document.getElementById('new-class-list-error');
+});
