c448f980b6ba69b696b9f0130ad80d6b734ccd95
smax253 <smax253@gmail.com>
2020-12-17 16:33:03 -0500

    Tag filters work on class [Resolves #222]

--- public/js/view_class.js
+++ public/js/view_class.js
@@ -1,10 +1,9 @@
 (function ($) {
     let params = new URLSearchParams(window.location.search);
-    
+
     const tagLinks = $('#class-tags li');
-    const origPosts = $('#post-list').html();
     const resetFilterButton = $('#reset-filter');
-    if(params.has('tag')){
+    if (params.has('tag')) {
         const postList = $('#post-list');
         const entries = $(origPosts).find('>li');
         const filteredPosts = entries.filter((ind, elem) => {
@@ -21,25 +20,35 @@
     }
     tagLinks.on('click', (event) => {
         event.preventDefault();
+
         const tag = event.target.text;
         const postList = $('#post-list');
-        const entries = $(origPosts).find('>li');
+        postList.hide();
+        const entries = postList.find('>li');
         const filteredPosts = entries.filter((ind, elem) => {
-            return elem.children.namedItem(tag) !== null;
+            const id = elem.id;
+            const tagItem = postList.find(`#${id}-${tag}`);
+            if (tagItem.length === 0) {
+                elem.hidden = true;
+            }
+            return tagItem.length !== 0;
         });
         if (filteredPosts.length === 0) {
-            postList.html(
-                '<p>There are no posts matching the selected filter.</p>'
-            );
-        } else {
-            postList.html(filteredPosts);
+            $('#no-posts-filter').show();
         }
+        postList.show();
+
         resetFilterButton.show();
     });
     resetFilterButton.on('click', (event) => {
         event.preventDefault();
         resetFilterButton.hide();
         const postList = $('#post-list');
-        postList.html($(origPosts));
+        const entries = postList.find('>li');
+        entries.each((ind, elem) => {
+            elem.hidden = false;
+        });
+        $('#no-posts-filter').hide();
+
     });
 })(window.jQuery);
